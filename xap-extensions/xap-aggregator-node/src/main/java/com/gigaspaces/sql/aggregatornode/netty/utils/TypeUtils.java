a49e5f0dc2a711431aba910adc14f262b584d392
yohanakh <yohana.khoury@gmail.com>
2021-07-26 02:16:51 +0300

    GS-14503 - fix Character type

--- xap-extensions/xap-aggregator-node/src/main/java/com/gigaspaces/sql/aggregatornode/netty/utils/TypeUtils.java
+++ xap-extensions/xap-aggregator-node/src/main/java/com/gigaspaces/sql/aggregatornode/netty/utils/TypeUtils.java
@@ -106,6 +106,8 @@ public class TypeUtils {
             case DOUBLE:
                 return PG_TYPE_FLOAT8;
             case CHAR:
+                if (internalType.getPrecision() == 0)
+                    return PG_TYPE_VARCHAR;
                 return PG_TYPE_CHAR;
             case VARCHAR:
                 return PG_TYPE_VARCHAR;
