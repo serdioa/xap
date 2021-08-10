bb991ecbc785f48b407e07eebe75b3e1c3d5b807
nivin <niv@gigaspaces.com>
2021-08-10 14:04:21 +0300

    GS-14557 port bug fix

--- xap-core/xap-common/src/main/java/com/gigaspaces/internal/services/RestServiceFactory.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/internal/services/RestServiceFactory.java
@@ -49,7 +49,9 @@ public class RestServiceFactory extends ServiceFactory {
                 .appendOptionalJars("metrics")
                 .appendOptionalJars("jdbc")
                 .appendPlatformJars("jdbc")
-                .appendPlatformJars("zookeeper");
+                .appendPlatformJars("zookeeper")
+                .appendJars(SystemLocations.singleton().libPlatformExt())
+        ;
 
         // Prevents ClassDef Exception while using kubernetes because he uses okhttp3 client (3.14.3) while we use okhttp2 (2.7.5) in other places.
         if (SystemInfo.singleton().getManagerClusterInfo().getManagerClusterType() == ManagerClusterType.ELASTIC_GRID) {
