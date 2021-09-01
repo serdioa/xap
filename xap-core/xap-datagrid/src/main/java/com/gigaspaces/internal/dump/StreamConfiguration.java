9334eb59e26d3f19a14429a1db808a1cca423a08
meshiruff <meshi.ruff@gigaspaces.com>
2021-09-01 15:21:58 +0300

    list of streams

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/dump/StreamConfiguration.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/dump/StreamConfiguration.java
@@ -1,5 +1,6 @@
 package com.gigaspaces.internal.dump;
 
+import com.gigaspaces.internal.io.IOUtils;
 import com.gigaspaces.serialization.SmartExternalizable;
 
 import java.io.IOException;
@@ -10,8 +11,23 @@ public class StreamConfiguration implements SmartExternalizable {
     private static final long serialVersionUID = 1L;
 
     private String streamName;
+    private String host;
 
     public StreamConfiguration() {
+    }
+
+    public StreamConfiguration(String streamName, String host) {
+        this.streamName = streamName;
+        this.host = host;
+    }
+
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
     }
 
     public String getStreamName() {
@@ -22,19 +38,18 @@ public class StreamConfiguration implements SmartExternalizable {
         this.streamName = streamName;
     }
 
-    public StreamConfiguration(String streamName) {
-        this.streamName = streamName;
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        IOUtils.writeString(out, streamName);
+        IOUtils.writeString(out, host);
     }
 
     @Override
-    public void writeExternal(ObjectOutput objectOutput) throws IOException {
-        objectOutput.writeUTF(streamName);
-
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        this.streamName =  IOUtils.readString(in);
+        this.host =  IOUtils.readString(in);
     }
 
-    @Override
-    public void readExternal(ObjectInput objectInput) throws IOException, ClassNotFoundException {
-        this.streamName = objectInput.readUTF();
 
-    }
+
 }
