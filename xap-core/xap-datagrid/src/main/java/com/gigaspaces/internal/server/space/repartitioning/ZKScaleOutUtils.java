cb516018d1d70a084b85e8f17eef8c758a1b9762
efratkimchi <efrat.kimchi@gigaspaces.com>
2021-10-10 14:27:12 +0300

    check tests

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/repartitioning/ZKScaleOutUtils.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/repartitioning/ZKScaleOutUtils.java
@@ -53,7 +53,7 @@ public class ZKScaleOutUtils {
        try {
            String status = getScaleOutDetails(attributeStore, puName, "status");
            if (status != null){
-               logger.info("Scale status for pu ["  + puName + "] is [" + status + "]");
+              // logger.info("Scale status for pu ["  + puName + "] is [" + status + "]");
                return ScaleStatus.IN_PROGRESS.getStatus().equals(status);
            }
        } catch (IOException e) {
