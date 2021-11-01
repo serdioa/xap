075bd545380bbf6c8403b6c9774f0b26f92205fd
mishel liberman <mishel170@gmail.com>
2021-11-01 17:19:13 +0200

    block dih-consumer blueprint from the cli

--- xap-extensions/xap-blueprints/src/main/java/org/gigaspaces/blueprints/Blueprint.java
+++ xap-extensions/xap-blueprints/src/main/java/org/gigaspaces/blueprints/Blueprint.java
@@ -20,6 +20,7 @@ public class Blueprint {
     private static final String TEMPLATES_PATH = "templates";
     private static final String INFO_PATH = "blueprint.yaml";
     private static final String VALUES_PATH = "values.yaml";
+    private static final String DIH_ROOT_PATH = "dih-consumer";
 
     private final String name;
     private final Path content;
@@ -42,7 +43,8 @@ public class Blueprint {
         return Files.exists(path) &&
                 Files.exists(path.resolve(TEMPLATES_PATH)) &&
                 Files.exists(path.resolve(INFO_PATH)) &&
-                Files.exists(path.resolve(VALUES_PATH));
+                Files.exists(path.resolve(VALUES_PATH)) &&
+                !path.endsWith(DIH_ROOT_PATH);
     }
 
     public static Collection<Blueprint> fromPath(Path path) throws IOException {
