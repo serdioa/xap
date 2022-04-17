d60a6f751fb66606ca72e56316bd4ae633806675
Evgeny Fisher <evgenyf@gmail.com>
2022-04-17 08:41:35 +0300

    IC-178 added serialVersionUID to new MissingConverterException class

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/dih/consumer/MissingConverterException.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/dih/consumer/MissingConverterException.java
@@ -2,6 +2,8 @@ package com.gigaspaces.dih.consumer;
 
 public class MissingConverterException extends MessageParsingException {
 
+    private static final long serialVersionUID = 1L;
+
     public MissingConverterException(String message, Exception e) {
         super(message,e);
     }
