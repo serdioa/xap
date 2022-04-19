71e02526fad81a93db38d58d0a50f86400d2af86
Anton.Soroka <anton.soroka@gigaspaces.com>
2022-04-19 22:20:27 +0300

    PIC-434 fixed behavior

--- xap-core/xap-openspaces/src/main/java/org/openspaces/log/RESTLogSeeker.java
+++ xap-core/xap-openspaces/src/main/java/org/openspaces/log/RESTLogSeeker.java
@@ -24,20 +24,46 @@ public class RESTLogSeeker implements ILogSeeker {
     public String find(StackTraceElement[] stackTrace) throws ClassNotFoundException {
         String className = null, methodName = null;
         for (StackTraceElement element : stackTrace) {
-            className = element.getClassName();
-            methodName = element.getMethodName();
-            Class<?> aClass = Class.forName(className);
-            if (aClass.getAnnotation(Controller.class) != null) {
-                for (Method method : aClass.getMethods()) {
+            String annotation = getString(element.getClassName(), element.getMethodName());
+            if (annotation != null) return annotation;
+        }
+        return null;
+    }
+
+    public String getString(String className, String methodName) throws ClassNotFoundException {
+        Class<?> aClass = Class.forName(className);
+        if (aClass.getAnnotation(Controller.class) != null) {
+            String prefix = "";
+            RequestMapping prefixReqestMapping = aClass.getAnnotation(RequestMapping.class);
+            if (prefixReqestMapping != null && prefixReqestMapping.value() != null && prefixReqestMapping.value().length>0) {
+                prefix = prefixReqestMapping.value()[0];
+                if (!prefix.endsWith("/")) {
+
+                }
+            }
+            do {
+                for (Method method : aClass.getDeclaredMethods()) {
                     if (method.getName().equals(methodName)) {
                         RequestMapping annotation = method.getAnnotation(RequestMapping.class);
                         if (annotation != null) {
-                            return "requestUrl=" + encodeSpecialSymbols(annotation.path().toString())
-                                    + " requestMethod=" + encodeSpecialSymbols(annotation.method().toString());
+                            String requestUrl = "";
+                            if (annotation.value() != null && annotation.value().length > 0) {
+                                requestUrl = annotation.value()[0];
+                            } else if (annotation.path() != null && annotation.path().length > 0) {
+                                requestUrl = annotation.path()[0];
+                            }
+                            requestUrl= prefix+requestUrl;
+                            String requestMethod = "";
+                            if (annotation.method() != null && annotation.method().length > 0) {
+                                requestMethod = annotation.method()[0].name();
+                            }
+                            return "requestUrl=" + encodeSpecialSymbols(requestUrl)
+                                    + " requestMethod=" + encodeSpecialSymbols(requestMethod);
                         }
                     }
                 }
-            }
+                aClass = aClass.getSuperclass();
+            } while (aClass != null);
         }
         return null;
     }
