192e6d4fd8133e6c6c2aa6511d3c052369c19136
sapirgila <sap4343@gmail.com>
2022-06-13 20:49:43 +0300

    PIC-728 added or and and complex criteria annotation

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/tiered_storage/CriteriaRangePredicate.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/tiered_storage/CriteriaRangePredicate.java
@@ -7,8 +7,6 @@ import com.j_spaces.core.cache.context.TemplateMatchTier;
 import com.j_spaces.jdbc.builder.range.ComposedRange;
 import com.j_spaces.jdbc.builder.range.Range;
 
-import java.util.ArrayList;
-
 public class CriteriaRangePredicate implements CachePredicate, InternalCachePredicate {
     private final String typeName;
     private final Range criteria;
@@ -48,24 +46,23 @@ public class CriteriaRangePredicate implements CachePredicate, InternalCachePred
     @Override
     public boolean evaluate(IEntryData entryData) {
         boolean result = false;
-        int j = 0;
         if (criteria instanceof ComposedRange) {
-            for (ArrayList path : ((ComposedRange) criteria).getPaths()) {
-                for (int i = 0; i < path.size(); i++) {
-                    Object field = (entryData.getFixedPropertyValue(entryData.getSpaceTypeDescriptor().getFixedPropertyPosition((String) path.get(i))));
-                    result = ((ComposedRange) criteria).get_ranges().get(j).getPredicate().execute(field);
-                    if (!result && !((ComposedRange) criteria).isUnion())
-                        return false;
-                    if (result && ((ComposedRange) criteria).isUnion())
-                        return true;
+            for (Range range : ((ComposedRange) criteria).getRanges()) {
+                if (range instanceof ComposedRange) {
+                    result = evalComposedPredicate((ComposedRange) range, entryData);
+                } else {
+                    Object value = (entryData.getFixedPropertyValue(entryData.getSpaceTypeDescriptor().getFixedPropertyPosition((range.getPath()))));
+                    result = range.getPredicate().execute(value);
                 }
-                j++;
+                if (result && ((ComposedRange) criteria).isUnion())
+                    return true;
+                if (!result && !((ComposedRange) criteria).isUnion())
+                    return false;
             }
-            //check what happens when last element - maybe if last- return last element's result.
+
         } else {
             Object field = (entryData.getFixedPropertyValue(entryData.getSpaceTypeDescriptor().getFixedPropertyPosition((((Range) criteria).getPath()))));
-            result = ((ComposedRange) criteria).get_ranges().get(0).getPredicate().execute(field);
-
+            result = criteria.getPredicate().execute(field);
         }
         return result;
     }
@@ -78,20 +75,19 @@ public class CriteriaRangePredicate implements CachePredicate, InternalCachePred
                 '}';
     }
 
-    private boolean evalComposedPredicate(IEntryData entryData) {
+    private boolean evalComposedPredicate(ComposedRange criteria, IEntryData entryData) {
 
-        int i = 0;
         boolean result = false;
-        for (ArrayList path : ((ComposedRange) criteria).getPaths()) {
-            for (i = 0; i < path.size(); i++) {
-                Object field = (entryData.getFixedPropertyValue(entryData.getSpaceTypeDescriptor().getFixedPropertyPosition((String) path.get(i))));
-                result = ((ComposedRange) criteria).get_ranges().get(i).getPredicate().execute(field);
-                if (!result && !((ComposedRange) criteria).isUnion())
-                    return false;
-                if (result && ((ComposedRange) criteria).isUnion())
-                    return true;
-            }
+        for (int i = 0; i < criteria.getRanges().size(); i++) {
+            Range range = criteria.getRanges().get(i);
+            Object value = (entryData.getFixedPropertyValue(entryData.getSpaceTypeDescriptor().getFixedPropertyPosition(range.getPath())));
+            result = range.getPredicate().execute(value);
+            if (!result && !criteria.isUnion())
+                return false;
+            if (result && criteria.isUnion())
+                return true;
+
         }
-        return true;
+        return result;
     }
 }
