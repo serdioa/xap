1efbb793440c142d54721ed96c9cbc298ae03716
mavigdor <moran@gigaspaces.com>
2022-11-29 11:12:41 +0200

    PIC-1342 Fix test failures after removal of xap charts prefix is now smart-cache; align usages

--- xap-core/xap-common/src/main/java/com/gigaspaces/start/ProductType.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/start/ProductType.java
@@ -12,4 +12,17 @@ public enum ProductType {
     public String getNameLowerCase() {
         return nameLowerCase;
     }
+
+    /**
+     * Modified name of XAP is smart-cache and is used as
+     * - zip file prefix
+     * - chart prefix
+     * - image repo prefix
+     *
+     * @return A Lower case modified name ('smart-cache' instead of 'xap'; 'insightedge' stays the same
+     * @since 16.3
+     */
+    public String toModifiedName() {
+        return this.equals(XAP) ? "smart-cache" : "insightedge";
+    }
 }
