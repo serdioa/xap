6ca54fe29bf91116e81014358eff095cb961abb7
Davyd Savitskyi <Davyd.Savitskyi@gigaspaces.com>
2023-02-20 13:55:56 +0200

    PIC-1709 - Add System info

---
+++ xap-core/xap-common/src/main/java/com/gigaspaces/start/security/SecurityServiceInfo.java
@@ -0,0 +1,35 @@
+package com.gigaspaces.start.security;
+
+import com.gigaspaces.start.kubernetes.KubernetesClusterInfo;
+
+public class SecurityServiceInfo {
+
+    private static volatile SecurityServiceInfo instance;
+    private final String DEFAULT_SECURITY_HOST = "localhost";
+    private final String DEFAULT_SECURITY_PORT = "9000";
+    private final String securityServiceBaseUrl;
+
+    public static SecurityServiceInfo getInstance(){
+        SecurityServiceInfo snapshot = instance;
+        if (snapshot != null)
+            return snapshot;
+        synchronized (SecurityServiceInfo.class) {
+            if (instance == null)
+                instance = new SecurityServiceInfo();
+            return instance;
+        }
+    }
+
+    private SecurityServiceInfo() {
+        this.securityServiceBaseUrl = String.format("http://%s:%s", validateUri(System.getenv("GS_SECURITY_SERVICE_HOST")), DEFAULT_SECURITY_PORT);
+    }
+
+
+    public String getSecurityServiceBaseUrl() {
+        return securityServiceBaseUrl;
+    }
+
+    private String validateUri(String s){
+        return s == null || s.isEmpty() || s.equals("null") ? DEFAULT_SECURITY_HOST : s;
+    }
+}
