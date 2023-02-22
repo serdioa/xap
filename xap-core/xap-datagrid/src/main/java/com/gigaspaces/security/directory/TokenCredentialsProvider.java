4cacb343063a38e56deff5aba25fe65a0286d005
Anton.Soroka <anton.soroka@gigaspaces.com>
2023-02-22 22:38:52 +0200

    Fixed issue with serialized and NPE at security filter

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/security/directory/TokenCredentialsProvider.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/security/directory/TokenCredentialsProvider.java
@@ -2,13 +2,17 @@ package com.gigaspaces.security.directory;
 
 import com.gigaspaces.security.Authority;
 
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
 public class TokenCredentialsProvider extends  CredentialsProvider {
 
     private static final long serialVersionUID = 1L;
 
     private TokenUserDetails tokenUserDetails;
 
-    protected TokenCredentialsProvider(){
+    public TokenCredentialsProvider(){
     }
 
     public TokenCredentialsProvider(final String token) {
@@ -39,4 +43,19 @@ public class TokenCredentialsProvider extends  CredentialsProvider {
     public UserDetails getUserDetails() {
         return tokenUserDetails;
     }
+
+    @Override
+    public void writeExternal(ObjectOutput out)
+            throws IOException {
+        super.writeExternal(out);
+        out.writeObject(tokenUserDetails);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in)
+            throws IOException, ClassNotFoundException {
+        super.readExternal(in);
+        this.tokenUserDetails = (TokenUserDetails) in.readObject();
+    }
+
 }
