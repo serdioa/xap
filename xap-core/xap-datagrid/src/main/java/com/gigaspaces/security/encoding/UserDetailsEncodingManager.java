ba10918d4e2da3e1114953989bcba60e05663950
alesiapavliuchenkova <alesia.pavliuchenkova@gigaspaces.com>
2023-04-27 18:06:53 +0300

    PIC-2266: Create userdetails encoding manager with the default password and content encoders

---
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/security/encoding/UserDetailsEncodingManager.java
@@ -0,0 +1,55 @@
+package com.gigaspaces.security.encoding;
+
+import com.gigaspaces.security.encoding.aes.AesContentEncoder;
+import com.gigaspaces.security.encoding.md5.Md5PasswordEncoder;
+
+public final class UserDetailsEncodingManager {
+
+    private final PasswordEncoder passwordEncoder;
+    private final ContentEncoder contentEncoder;
+
+    /**
+     * Constructs the encoders
+     */
+    public UserDetailsEncodingManager() {
+        passwordEncoder = createPasswordEncoder();
+        contentEncoder = createContentEncoder();
+    }
+
+    /**
+     * @return a PasswordEncoder implementation {@link Md5PasswordEncoder}.
+     */
+    private PasswordEncoder createPasswordEncoder() throws EncodingException {
+        try {
+            Class<? extends PasswordEncoder> passwordEncoderClass = Class.forName(Md5PasswordEncoder.class.getName())
+                    .asSubclass(PasswordEncoder.class);
+            return passwordEncoderClass.newInstance();
+        } catch (Exception e) {
+            throw new EncodingException("Failed to create PasswordEncoder", e);
+        }
+    }
+
+    /**
+     * @return a ContentEncoder implementation {@link AesContentEncoder}
+     */
+    private ContentEncoder createContentEncoder() throws EncodingException {
+        try {
+            Class<? extends ContentEncoder> contentEncoderClass = Class.forName(AesContentEncoder.class.getName())
+                    .asSubclass(ContentEncoder.class);
+
+            return contentEncoderClass.newInstance();
+
+        } catch (Exception e) {
+            throw new EncodingException("Failed to create ContentEncoder", e);
+        }
+    }
+
+    public PasswordEncoder getPasswordEncoder() {
+        return passwordEncoder;
+    }
+
+    public ContentEncoder getContentEncoder() {
+        return contentEncoder;
+    }
+
+}
