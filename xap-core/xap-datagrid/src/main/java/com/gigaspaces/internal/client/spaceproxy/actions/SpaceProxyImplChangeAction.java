122087e7c058cbfb565593634e8ea68809c07dad
olha-rymarchuk <olha.rymarchuk@gigaspaces.com>
2023-06-02 15:00:33 +0300

    - block change with lease for mvcc; - add change with lease operation to MvccUpdateOperationWithLeaseValidationTest;

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/client/spaceproxy/actions/SpaceProxyImplChangeAction.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/client/spaceproxy/actions/SpaceProxyImplChangeAction.java
@@ -24,6 +24,7 @@ import com.gigaspaces.internal.client.spaceproxy.operations.ChangeEntriesSpaceOp
 import com.j_spaces.core.client.Modifiers;
 import com.j_spaces.jdbc.builder.SQLQueryTemplatePacket;
 
+import net.jini.core.lease.Lease;
 import net.jini.core.transaction.TransactionException;
 
 import java.rmi.RemoteException;
@@ -52,6 +53,11 @@ public class SpaceProxyImplChangeAction
                 actionInfo.modifiers,
                 actionInfo.mutators,
                 actionInfo.getQuery());
+
+        if (spaceProxy.getProxySettings().isMvccEnabled() && request.getLease() != Lease.FOREVER) {
+            throw new UnsupportedOperationException("Change operation with lease are not allowed when MVCC is enabled.");
+        }
+
         if (Modifiers.contains(actionInfo.modifiers, Modifiers.ONE_WAY)) {
             spaceProxy.getProxyRouter().executeOneway(request);
             return null;
