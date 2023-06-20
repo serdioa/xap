15a3ead710c97a276ab8ec1c9a0e772a2b2cb1ad
esteratz <ester.atzmon@gigaspaces.com>
2023-06-20 10:35:43 +0300

    GS-14951 - translate strip property once.

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/utils/TransformUtils.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/utils/TransformUtils.java
@@ -5,9 +5,10 @@ import com.j_spaces.kernel.SystemProperties;
 import java.math.BigDecimal;
 
 public class TransformUtils {
+    public static boolean strip = SystemProperties.getBoolean(SystemProperties.BIG_DECIMAL_STRIP_TRAILING_ZEROS, SystemProperties.BIG_DECIMAL_STRIP_TRAILING_ZEROS_DEFAULT);
     public static Object stripTrailingZerosIfNeeded(Object value){
         Object val = value;
-        if (SystemProperties.getBoolean(SystemProperties.BIG_DECIMAL_STRIP_TRAILING_ZEROS, SystemProperties.BIG_DECIMAL_STRIP_TRAILING_ZEROS_DEFAULT)) {
+        if (strip) {
             val = value instanceof BigDecimal ? ((BigDecimal) value).stripTrailingZeros() : value;
         }
         return val;
