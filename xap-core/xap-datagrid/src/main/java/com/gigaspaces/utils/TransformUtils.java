85c2558844df649f7495eedbca4e33133f676017
tomershapira-gs <79213935+tomershapira-gs@users.noreply.github.com>
2023-07-23 14:43:52 +0300

    PIC-2840 - after review

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/utils/TransformUtils.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/utils/TransformUtils.java
@@ -1,6 +1,5 @@
 package com.gigaspaces.utils;
 
-import com.gigaspaces.entry.CompoundSpaceId;
 import com.j_spaces.kernel.SystemProperties;
 
 import java.math.BigDecimal;
@@ -9,14 +8,7 @@ public class TransformUtils {
     public static boolean strip = SystemProperties.getBoolean(SystemProperties.BIG_DECIMAL_STRIP_TRAILING_ZEROS, SystemProperties.BIG_DECIMAL_STRIP_TRAILING_ZEROS_DEFAULT);
     public static Object stripTrailingZerosIfNeeded(Object value){
         if (strip) {
-            if (value instanceof CompoundSpaceId){
-                CompoundSpaceId cid = (CompoundSpaceId) value;
-                for (int i = 0; i < cid.length(); i++) {
-                    cid.setValue(i, stripTrailingZerosIfNeeded(cid.getValue(i)));
-                }
-            } else {
-                value = value instanceof BigDecimal ? ((BigDecimal) value).stripTrailingZeros() : value;
-            }
+            value = value instanceof BigDecimal ? ((BigDecimal) value).stripTrailingZeros() : value;
         }
         return value;
     }
