a5a501e904cc0208436d2b349174d78ec756c07e
davydsav <108653379+davydsav@users.noreply.github.com>
2023-07-26 12:46:37 +0300

    PIC-2847 - Implement cleaning background thread (#430)
    * PIC-1540 - Refactor after review
    
    * PIC-2848 MVCCCleanUp space properties
    
    * PIC-2846 - Configure space props
    
    * PIC-2846 - Changes after review
    
    * PIC-2847 - Add cleaner thread
    
    * PIC-2846 - Throw an exception on zk is not available
    
    * PIC-2846 - Add MVCC cleaner thread / gracefully terminate
    
    * PIC-2846 - Refactor
    
    * PIC-2847 - Added log
    
    ---------
    
    Co-authored-by: Sagiv Michael <Sagiv07@gmail.com>

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/mvcc/MVCCCleanupManager.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/mvcc/MVCCCleanupManager.java
@@ -3,9 +3,12 @@ package com.gigaspaces.internal.server.space.mvcc;
 import com.gigaspaces.internal.server.space.SpaceEngine;
 import com.gigaspaces.internal.server.space.SpaceImpl;
 import com.gigaspaces.internal.server.space.ZooKeeperMVCCInternalHandler;
+import com.gigaspaces.internal.utils.concurrent.GSThread;
 import com.j_spaces.core.admin.SpaceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Davyd Savitskyi
@@ -17,10 +20,10 @@ public class MVCCCleanupManager {
     private static final Logger _logger = LoggerFactory.getLogger(com.gigaspaces.logger.Constants.LOGGER_MVCC_CLEANUP);
 
     private final SpaceImpl _spaceImpl;
-    private SpaceEngine _engine;
     private final SpaceConfig _spaceConfig;
     private final ZooKeeperMVCCInternalHandler _zookeeperMVCCHandler;
 
+    private MVCCGenerationCleaner _mvccCleanerDaemon;
     private boolean _closed;
 
 
@@ -42,19 +45,90 @@ public class MVCCCleanupManager {
     */
     public void init() {
         if (!_closed) {
-            _engine = _spaceImpl.getEngine();
-            _logger.debug("MVCC cleaner daemon thread started");
-            //TODO: in PIC-2847 - init cleaner thread and start
+            _mvccCleanerDaemon = new MVCCGenerationCleaner(MVCCGenerationCleaner.class.getSimpleName() + "-" + _spaceImpl.getName());
+            _mvccCleanerDaemon.start();
+            _logger.debug("MVCC cleaner daemon " + _mvccCleanerDaemon.getName() +  " started");
         }
     }
 
     /**
-     * Closes the MVCC Cleanup Manager (gracefully die cleaner daemon thread)
+     * Closes the MVCC Cleanup Manager (gracefully terminate cleaner daemon thread)
      */
     public final void close() {
         _closed = true;
-        // TODO: in PIC-2847 - gracefully terminate cleaner thread
-        _logger.debug("MVCC cleaner daemon thread terminated");
+        if (_mvccCleanerDaemon != null) {
+            _mvccCleanerDaemon.terminate();
+        }
+        _logger.debug("MVCC cleanup manager closed");
+    }
+
+    private final class MVCCGenerationCleaner extends GSThread {
+
+        // TODO: use consts as defaults to impl adaptive clean timeout logic (PIC-2850)
+        private final long MIN_CLEANUP_DELAY_INTERVAL_MILLIS = TimeUnit.SECONDS.toMillis(1);
+        private final long MAX_CLEANUP_DELAY_INTERVAL_MILLIS = TimeUnit.MINUTES.toMillis(1);
+        private final long INITIAL_CLEANUP_INTERVAL_MILLIS = TimeUnit.SECONDS.toMillis(1);
+
+        private boolean _shouldTerminate;
+        private long _nextCleanupDelayInterval;
+        // TODO: use to calculate _nextCleanupDelayInterval (PIC-2850)
+        private long _lastCleanupExecutionInterval;
+        private long _lastCleanupExecutionTimestamp;
+
+        public MVCCGenerationCleaner(String name) {
+            super(name);
+            this.setDaemon(true);
+            _nextCleanupDelayInterval = MIN_CLEANUP_DELAY_INTERVAL_MILLIS;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!_shouldTerminate) {
+                    try {
+                        fallAsleep();
+                        cleanExpiredEntriesGenerations();
+                    } catch (Exception ex) {
+                        _logger.error(this.getName() + " - caught Exception", ex);
+                    }
+                }
+            } finally {
+                if (_logger.isDebugEnabled())
+                    _logger.debug("MVCC cleaner " + this.getName() + " terminated.");
+            }
+        }
+
+        private synchronized void fallAsleep() throws InterruptedException {
+            if (_shouldTerminate) {
+                return;
+            }
+            if (_logger.isDebugEnabled())
+                _logger.debug("fallAsleep - going to wait cleanupDelay=" + _nextCleanupDelayInterval);
+            wait(_nextCleanupDelayInterval);
+        }
+
+        private void cleanExpiredEntriesGenerations() {
+            if (_shouldTerminate) {
+                return;
+            }
+            MVCCGenerationsState genState = _zookeeperMVCCHandler.getGenerationsState();
+            if (_logger.isDebugEnabled()) {
+                _logger.debug("MVCC cleanup started with last generation: " + genState);
+            }
+            // TODO: implement cleanup logic (PIC-2849)
+
+        }
+
+        public void terminate() {
+            if (!isAlive())
+                return;
+
+            synchronized (this) {
+                _shouldTerminate = true;
+                // if daemon is waiting between cleanups -> wake it up
+                notify();
+            }
+        }
     }
 
 
