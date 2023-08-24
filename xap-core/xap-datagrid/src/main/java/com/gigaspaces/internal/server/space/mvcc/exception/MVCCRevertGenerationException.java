fe1d628ffcb9f2cb8a5f699e173871826018f1c7
Sagiv Michael <Sagiv07@gmail.com>
2023-08-24 11:48:29 +0300

    add missing serialVersionUID to MVCCRevertGenerationException

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/mvcc/exception/MVCCRevertGenerationException.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/mvcc/exception/MVCCRevertGenerationException.java
@@ -2,6 +2,8 @@ package com.gigaspaces.internal.server.space.mvcc.exception;
 
 public class MVCCRevertGenerationException extends MVCCGenerationConflictRuntimeException {
 
+    private static final long serialVersionUID = -602018298200803915L;
+
     public MVCCRevertGenerationException(String message) {
         super(message);
     }
