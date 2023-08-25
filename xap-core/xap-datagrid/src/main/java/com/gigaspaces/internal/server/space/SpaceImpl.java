cf535bfb52ca022f5458aa625dbea9297b44ef36
Anton.Soroka <anton.soroka@gigaspaces.com>
2023-08-25 11:36:42 +0300

    PIC-3014: add logger

--- xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/SpaceImpl.java
+++ xap-core/xap-datagrid/src/main/java/com/gigaspaces/internal/server/space/SpaceImpl.java
@@ -2858,6 +2858,9 @@ public class SpaceImpl extends AbstractService implements IRemoteSpace, IInterna
             SpaceActionExecutor executor = getActionExecutor(systemTask);
             supportsBackup = executor.supportsBackup();
             privilege = executor.getPrivilege();
+            _logger.info("system task used context " + sc);
+        } else {
+            _logger.info("none system task used context " + sc);
         }
 
         beforeTypeOperation(!supportsBackup, sc, privilege, typeName);
@@ -2866,6 +2869,8 @@ public class SpaceImpl extends AbstractService implements IRemoteSpace, IInterna
 
         if (!isSystemTask)
             _engine.invokeFilters(sc, FilterOperationCodes.BEFORE_EXECUTE, task);
+
+        _logger.info("after invoking filters");
 
         IDirectSpaceProxy spaceProxy = getTaskProxy().getDirectProxy();
         SpaceContext prevContext = null;
@@ -2878,8 +2883,13 @@ public class SpaceImpl extends AbstractService implements IRemoteSpace, IInterna
                 xtnEntry.setOperatedUpon();
             }
             if (isSecuredSpace()) {
-                _logger.info("Change space context to trust context...");
-                prevContext = spaceProxy.getSecurityManager().setThreadSpaceContext(_securityInterceptor.trustContext(sc)); // TODO : wrong context ?
+                _logger.info("Change space context " + sc);
+
+                SpaceContext trustContext = _securityInterceptor.trustContext(sc);
+
+                _logger.info("To trusted context " + trustContext);
+
+                prevContext = spaceProxy.getSecurityManager().setThreadSpaceContext(trustContext); // TODO : wrong context ?
             }
             Object result = task.execute(spaceProxy, tx);
             if (!isSystemTask && _engine.getFilterManager()._isFilter[FilterOperationCodes.AFTER_EXECUTE])
@@ -2890,8 +2900,11 @@ public class SpaceImpl extends AbstractService implements IRemoteSpace, IInterna
         } catch (Exception e) {
             throw new ExecutionException(e);
         } finally {
-            if (isSecuredSpace())
+            if (isSecuredSpace()) {
+                _logger.info("Change security context from final of SpaceImpl");
                 spaceProxy.getSecurityManager().setThreadSpaceContext(prevContext);
+            }
+
 
             // Unload the task class.
             // See org.openspaces.core.executor.internal.InternalSpaceTaskWrapper#readTaskInNewClassLoader(ObjectInput in).
