b153ff3f4980b74915ff90dde417c06d5e7338f3
Oleksii Vedernikov <oleksii.vedernikov@gigaspaces.com>
2023-10-20 13:57:48 +0200

    fix - tokeService loading conditions fixed

--- xap-core/xap-common/src/main/java/com/gigaspaces/start/SystemConfig.java
+++ xap-core/xap-common/src/main/java/com/gigaspaces/start/SystemConfig.java
@@ -332,10 +332,10 @@ public class SystemConfig {
         classpathBuilder.appendOptionalJars("tiered-storage/sqlite");
         classpathBuilder.appendOptionalJars("data-integration");
         classpathBuilder.appendPlatformJars("jdbc");
+        classpathBuilder.appendOptionalJars("security/xap-token-service.jar");
 
         if (SystemInfo.singleton().security().isOpenIdSecurityManager()) {
             classpathBuilder.appendOptionalJars("security/xap-security.jar");
-            classpathBuilder.appendOptionalJars("security/xap-token-service.jar");
             classpathBuilder.appendRequiredJar("spring-security-oauth2-jose.jar");
             classpathBuilder.appendRequiredJar("spring-security-oauth2-resource-server.jar");
             classpathBuilder.appendRequiredJar("spring-security-oauth2-core.jar");
